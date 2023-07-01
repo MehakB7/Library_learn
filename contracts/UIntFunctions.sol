7e17cd288172af9b89b955e748778ba4274a0ac6
MehakB77 <87482443+MehakB7@users.noreply.github.com>
2023-07-01 22:31:21 +0530

    added divide evenly function in library

--- contracts/UIntFunctions.sol
+++ contracts/UIntFunctions.sol
@@ -5,4 +5,8 @@ library UIntFunctions {
     function isEven(uint number) public pure returns (bool) {
         return number % 2 == 0;
     }
+
+    function dividesEvenly(uint num1, uint num2) external pure returns (bool) {
+        return num1 % num2 == 0;
+    }
 }
