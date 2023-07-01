10201d10b6a4db2b26a90a009fe93e3a146262bb
MehakB77 <87482443+MehakB7@users.noreply.github.com>
2023-07-01 22:24:49 +0530

    using library function

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -6,4 +6,9 @@ import "./UIntFunctions.sol";
 contract Game {
     uint public participants;
     bool public allowTeams;
+
+    constructor(uint _participants) {
+        participants = _participants;
+        allowTeams = UIntFunctions.isEven(_participants);
+    }
 }
